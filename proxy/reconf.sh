b53b493090c8c3cb2061469a485f7a45f135ca70
liulang <lazebird@gmail.com>
2019-11-18 16:11:24 +0800

    add proxy reconf.sh

---
+++ proxy/reconf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# remove game server
+sed -i '/游戏.*type/d' config.yml
+sed -i 's/, "[^"]*游戏[^"]*"//g' config.yml
+# allow lan
+sed -i 's/allow-lan: false/allow-lan: true/' config.yml
+# use auto group instead
+sed -i 's/- { name: MunCloud-旗舰加速-Diamond Package-new/#- { name: MunCloud-旗舰加速-Diamond Package-new/' config.yml
+sed -i 's/#- { name: auto/- { name: MunCloud-旗舰加速-Diamond Package-new/' config.yml
+# add rule
+sed -i 's/- GEOIP/- DOMAIN-SUFFIX,baidu.com,DIRECT\n- GEOIP/' config.yml
+sed -i 's/- GEOIP/- DOMAIN-SUFFIX,sina.com,DIRECT\n- GEOIP/' config.yml
+sed -i 's/- GEOIP/- DOMAIN-SUFFIX,163.com,DIRECT\n- GEOIP/' config.yml
+sed -i 's/- GEOIP/- DOMAIN-SUFFIX,qq.com,DIRECT\n- GEOIP/' config.yml
+sed -i 's/- GEOIP/- DOMAIN-SUFFIX,taobao.com,DIRECT\n- GEOIP/' config.yml
+sed -i 's/- GEOIP/- DOMAIN-SUFFIX,jd.com,DIRECT\n- GEOIP/' config.yml
+sed -i 's/- GEOIP/- IP-CIDR,192.0.0.0\/8,DIRECT\n- GEOIP/' config.yml
