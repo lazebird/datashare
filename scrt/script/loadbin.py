3be96dad4b912a2c4d80fa0d61a84562f31c31b3
lazebird <lazebird@gmail.com>
2019-12-04 17:55:32 +0800

    more

--- scrt/script/loadbin.py
+++ scrt/script/loadbin.py
@@ -1,40 +1,32 @@
-prog = crt.Screen.ReadString("\r\n","?", "^C")
+prog = crt.Screen.ReadString({"\r\n","?", "^C"})
 restart_mode = "none"  # mode: restart reboot none
 
-modhash = new Object()
-modfilename = "D:#restart_mode.txt"
+modhash = {}
+modfilename = "D:/restart_mode.txt"
 def  loadfile(filename): 
-	fso, f           
-	fso = new ActiveXObject("Scripting.FileSystemObject")
-	if not fso.FileExists(filename): 
-		return
-	
-	f = fso.OpenTextFile(filename, 1) #ForReading = 1, ForWriting = 2 
-	while  f.AtEndOfStream != true :            
-	   str = f.Readline()
-	   params=str.split(' ')
-	   modhash[params[0]] = params[1]
-	
-	f.Close()  
+	try :
+		f = open(filename, "r")
+		for line in f.readlines():
+			line = line.strip()
+			params=line.split(' ')
+			modhash[params[0]] = params[1]
+		f.close()  
+	except:
+		crt.Screen.Send("#File "+filename+" not exists")
 
 
 def  writefile(filename): 
-	fso, f           
-	ForReading = 1, ForWriting = 2 
-	fso = new ActiveXObject("Scripting.FileSystemObject")          
-	f = fso.OpenTextFile(filename, 2, 1) 
-	for (key in modhash) 
-		f.Write(key+" "+modhash[key]+"\n")     
-	
-	f.Close()  
+	f = open(filename, "w") 
+	for key in modhash:
+		f.write(key+" "+modhash[key]+"\n")     
+	f.close()  
 
 
 loadfile(modfilename)
 
-args = ()
-prog = prog.replace(new RegExp("#","gm"), "")
+prog = prog.replace("#", ",")
 args = prog.split(",")
-if args[1]:  # input string has args
+if len(args) > 1:  # input string has args
 	prog = args[0]
 	restart_mode = args[1]
 	modhash[prog] = restart_mode
@@ -44,29 +36,27 @@ else:
 	
 
 
-if(crt.Screen.MatchIndex != 3 and prog != "") 
+if crt.Screen.MatchIndex != 3 and prog != "":
 	crt.Screen.Send("\3")
 	crt.Screen.WaitForString("root", 1)
-    crt.Screen.Send("wget http://192.168.1.106/bin/"+prog+"\r\n")
-    ret = crt.Screen.WaitForStrings(["saved","failed", "ERROR", "No such", "^C"])
-    if(ret == 1) 
-        crt.Screen.Send("chmod 777 "+prog+"\r\n")
-        crt.Screen.Send("mv /usr/bin/"+prog+" /"+prog+".bak\r\n")
-        crt.Screen.Send("mv "+prog+" /usr/bin\r\n")
+	crt.Screen.Send("wget http://192.168.1.106/bin/"+prog+"\r\n")
+	ret = crt.Screen.WaitForStrings(["saved","failed", "ERROR", "No such", "^C"])
+	if ret == 1:
+		crt.Screen.Send("chmod 777 "+prog+"\r\n")
+		crt.Screen.Send("mv /usr/bin/"+prog+" /"+prog+".bak\r\n")
+		crt.Screen.Send("mv "+prog+" /usr/bin\r\n")
 		crt.Screen.Send("sync\r\n")
 		crt.Screen.WaitForStrings(["root", "^C"])
 		if restart_mode == "restart": 
 			crt.Screen.Send("pkill "+prog+"\r\n")
 			crt.Screen.WaitForString("root", 1)
 			crt.Screen.Send(prog+" &\r\n")
-		 elif restart_mode == "reboot": 
+		elif restart_mode == "reboot": 
 			crt.Screen.Send("reboot\r\n")
-		 elif restart_mode == "none": 
+		elif restart_mode == "none": 
 			crt.Screen.Send("\3\r\n#please deal with it ASAP.\r\n")
-		
-    else: 
+	else: 
 		crt.Screen.Send("\3\r\n#something is wrong, ret "+ret+"\r\n")
-	
 else: 
 	crt.Screen.Send("\3\r\n#input format: \"progname [,mode]?\", mode: restart reboot none\r\n")
 	crt.Screen.Send("\3\r\n#script terminated for u.\r\n")
