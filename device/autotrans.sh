3e37fa78c53d15d5bf9ecbe68cff11ab2c89257b
lazebird <lazebird@gmail.com>
2020-02-28 19:09:36 +0800

    more

--- device/autotrans.sh
+++ device/autotrans.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-node autotrans.js | jq . >translat.jsn.json
+node autotrans.js | jq --indent 3 . >translat.jsn.json
 if [ "$(pwd)" == "/root/projects/datashare/device" ]; then
     cp translat.jsn ../../robo/src/appl/web/web_root/en/translat.jsn
     cp translat.jsn.json ../../robo/src/appl/web/web_root/cn/translat.jsn
