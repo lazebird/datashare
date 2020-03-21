e0f435e28a5c94a9c355f98e394f06b47fc4057a
lazebird <lazebird@gmail.com>
2020-03-21 11:36:16 +0800

    more

--- proxy/reconf.sh
+++ proxy/reconf.sh
@@ -8,7 +8,7 @@ sed -i 's/, "[^"]*游戏[^"]*"//g' $filename
 # allow lan
 sed -i 's/allow-lan: false/allow-lan: true/' $filename
 # use auto group instead
-sed -i -e ":begin; /name: Proxy/,/type: select/ { /type: select/! { $! { N; b begin }; }; s/name: Proxy\n  type: select/name: Proxy\n  type: url-test\n  url: http:\/\/www\.gstatic\.com\/generate_204\n  interval: 300/; };" $filename
+sed -i -e ":begin; /节点选择/,/type: select/ { /type: select/! { $! { N; b begin }; }; s/节点选择\n    type: select/节点选择\n    type: url-test\n    url: http:\/\/www\.gstatic\.com\/generate_204\n    interval: 300/; };" $filename
 # add rule
 sed -i 's/- GEOIP/- DOMAIN-SUFFIX,baidu.com,DIRECT\n- GEOIP/' $filename
 sed -i 's/- GEOIP/- DOMAIN-SUFFIX,youku.com,DIRECT\n- GEOIP/' $filename
