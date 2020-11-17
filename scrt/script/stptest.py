36ad1a22e7a7107e5117bab1642ae187cb487b3e
lazebird <lazebird@gmail.com>
2020-11-17 10:27:52 +0800

    more

--- scrt/script/stptest.py
+++ scrt/script/stptest.py
@@ -5,6 +5,7 @@ import random
 (strScriptPath, strScriptName) = os.path.split(__file__)
 if strScriptPath not in sys.path:
 	sys.path.insert(0, strScriptPath)
+sys.dont_write_bytecode = True
 
 import devutils
 
--- scrt/script/syspath.py
+++ scrt/script/syspath.py
@@ -4,4 +4,7 @@ import os
 (strScriptPath, strScriptName) = os.path.split(__file__)
 if strScriptPath not in sys.path:
     sys.path.insert(0, strScriptPath)
+sys.dont_write_bytecode = True
+
 crt.Dialog.MessageBox("sys.path: \n" + "\n".join(sys.path))
+
