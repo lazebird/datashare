33e1efb9688cf50496442dbf18d28e7962373577
lazebird <lazebird@gmail.com>
2020-12-07 19:32:59 +0800

    more

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -1,36 +1,54 @@
-cat > updater.sh <<EOF && sh updater.sh
 #!/bin/sh
 
-echo "#### this is an updater for proxy"
-
-# parameter: filename, path, url
-fetch_file() {
-    filename=\$1
-    path=\$2
-    url=\$3 
-    if [ -f \$path/\$filename ]; then
-        echo "# \$path/\$filename already exists"
-        return
-    fi
-    wget -O \$path/\$filename \$url || exit 1
+print_exit() { # parameter: logstr
+    echo "$1" >&2 && exit 1
 }
 
-BASHDIR="\$(cd "\$(dirname "\${BASH_SOURCE[0]}")" && pwd)"
+fetch_file() { # parameter: filename, path, url
+    filename=$1
+    path=$2
+    url=$3
+    if [ -f $path/$filename ]; then
+        echo "# $path/$filename already exists."
+        return
+    fi
+    wget -q -O $path/$filename $url >/dev/null || print_exit "# $path/$filename download failed."
+    echo "# $path/$filename download success."
+}
+
+get_clash() {
+    arch=$(uname -m)
+    case $arch in
+    x86_64)
+        fetch_file clash $workdir "https://code.aliyun.com/lazebird/datashare/raw/master/proxy/clash-linux-amd64"
+        ;;
+    mips)
+        fetch_file clash $workdir "https://code.aliyun.com/lazebird/datashare/raw/master/proxy/clash-linux-mipsle-hardfloat"
+        ;;
+    *)
+        echo "# unsupportted arch $arch."
+        ;;
+    esac
+}
+
+[ -z "${BASH_SOURCE}" ] && SCRIPTPATH=$0 || SCRIPTPATH=${BASH_SOURCE[0]}
+BASHDIR="$(cd "$(dirname "$SCRIPTPATH")" && pwd)"
+workdir=${1-"/tmp"}
 configname=config.yaml
-workdir=\$BASHDIR
-configurl="$1" # secret
-cd \$workdir
-echo "#### updating configure files"
-mv \$configname \$configname".bak" 2>/dev/null # force update
-fetch_file \$configname \$workdir \$configurl
-fetch_file clash \$workdir "https://code.aliyun.com/lazebird/datashare/raw/master/proxy/clash-linux-mipsle-hardfloat"
-fetch_file reconf.sh \$workdir "https://code.aliyun.com/lazebird/datashare/raw/master/proxy/reconf.sh"
-fetch_file Country.mmdb \$workdir "https://code.aliyun.com/lazebird/datashare/raw/master/proxy/Country.mmdb"
-echo "#### processing configure files"
-cd \$workdir && chmod 777 *.sh clash
-sed -i 's/\r//g' \$configname # dos2unix \$configname # may not exist, use sed instead?
-./reconf.sh \$configname
-kill \`pgrep clash\` 2>/dev/null && sleep 1s
-./clash -d . &
+configurl=$(cat $BASHDIR/proxyurl.txt) # secret
+[ -z "$configurl" ] && print_exit "#### config url invalid!"
+
+cd $workdir
+echo "#### updating configure files in $workdir:"
+mv $configname $configname".bak" 2>/dev/null # force update
+fetch_file $configname $workdir $configurl
+fetch_file reconf.sh $workdir "https://code.aliyun.com/lazebird/datashare/raw/master/proxy/reconf.sh"
+fetch_file Country.mmdb $workdir "https://code.aliyun.com/lazebird/datashare/raw/master/proxy/Country.mmdb"
+get_clash
+echo "#### processing configure files in $workdir:"
+cd $workdir && chmod +x reconf.sh clash
+sed -i 's/\r//g' $configname # dos2unix $configname # may not exist, use sed instead?
+./reconf.sh $configname || print_exit "# reconf $configname failed."
+kill \`pgrep clash\` 2>/dev/null && sleep 3s
+./clash -d . >proxy.log &
 echo "#### update successfully!"
-EOF
