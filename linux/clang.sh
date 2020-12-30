bc91d5c347dbba83be9782747b18b7ed12e38b8b
lazebird <lazebird@gmail.com>
2020-12-30 10:25:45 +0800

    more

--- linux/clang.sh
+++ linux/clang.sh
@@ -3,8 +3,11 @@
 output=${1:-~/.clang-format}
 
 clang-format -style=llvm -dump-config >$output
-sed -i 's/ColumnLimit:.*/ColumnLimit: 150/' $output
-sed -i 's/^IndentWidth:.*/IndentWidth: 4/' $output
+sed -i 's/ColumnLimit:.*/ColumnLimit:     180/' $output
+sed -i 's/^IndentWidth:.*/IndentWidth:     4/' $output
+sed -i 's/AllowShortBlocksOnASingleLine:.*/AllowShortBlocksOnASingleLine: true/' $output
+sed -i 's/AllowShortLoopsOnASingleLine:.*/AllowShortLoopsOnASingleLine: true/' $output
+sed -i 's/BreakBeforeBraces:.*/BreakBeforeBraces: Linux/' $output
 sed -i 's/AllowShortFunctionsOnASingleLine:.*/AllowShortFunctionsOnASingleLine: false/' $output
 sed -i 's/AllowShortIfStatementsOnASingleLine:.*/AllowShortIfStatementsOnASingleLine: true/' $output
-sed -i 's/SortIncludes:.*/SortIncludes: false/' $output
+sed -i 's/SortIncludes:.*/SortIncludes:    false/' $output
