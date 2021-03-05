f60838c5e34834e2830056674248e879f127e634
lazebird <lazebird@gmail.com>
2021-03-05 11:21:22 +0800

    more

--- scrt/script/bug_ps.py
+++ scrt/script/bug_ps.py
@@ -38,10 +38,10 @@ def	cmdloop():
 while cmdloop() and sess.cmdreboot() and sess.wait2login("admin", "admin", False):pass
 errmsg = "#"+sess.name+" game over, count "+str(count)+", cause "+cause+"!"
 if cause == "unknown": # dump stacks
-	crt.Screen.sendspecial("TN_BREAK")
+	crt.Screen.SendSpecial("TN_BREAK")
 	crt.Screen.Send("l") # Shows a stack backtrace for all active CPUs.	
 	crt.Sleep(1)
-	crt.Screen.sendspecial("TN_BREAK")
+	crt.Screen.SendSpecial("TN_BREAK")
 	crt.Screen.Send("t") # Output a list of current tasks and their information to the console	
 crt.Screen.Send(errmsg+"\n")
 crt.Dialog.MessageBox(errmsg)
