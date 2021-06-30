74553216c20f9dd0d5956e336a4c5928273bf5a4
lazebird <lazebird@gmail.com>
2021-06-30 16:39:20 +0800

    more

--- scrt/script/produce.py
+++ scrt/script/produce.py
@@ -6,6 +6,8 @@ sn = ""
 mac = ""
 pid = ""
 ver = ""
+max_mac = ""
+max_mac_post = 0xFFFFFF
 
 
 def debug(s):
@@ -13,14 +15,33 @@ def debug(s):
 
 
 def args2str():
-    return "SN=" + sn + "\nMAC=" + mac + "\nID=" + pid + "\nVER=" + ver
+    return "SN=" + sn + "\nMAC=" + mac + "\nID=" + pid + "\nVER=" + ver + "\nMAX_MAC=" + max_mac
 
 
-def parse_opt(obj):
-    opthash = {}
+def property_file2line(filepath):
+    s = ""
+    try:
+        f = open(filepath, "r")
+        for line in f.readlines():
+            line = line.strip().replace(" ", "")
+            line = line if (line.find("#") < 0) else line[0 : line.find("#")]  # add comment support
+            s = s + line + " "
+        f.close()
+    except:
+        debug("File " + filepath + " not exists")
+    return s
+
+
+def cmdargs2line(obj):
     s = ""
     for index in range(obj.Count):
         s = s + obj[index] + " "
+    return s
+
+
+def opt_parse(s):
+    opthash = {}
+    s = s.replace('"', "")  # strip extra \"
     s = s.replace("#", " ")  # support '#' as ' '
     s = s.replace(",", " ")  # support ',' as ' '
     s = s.replace("  ", " ")  # support '  ' as ' '
@@ -35,6 +56,21 @@ def parse_opt(obj):
     return opthash
 
 
+def opts2args(opthash):
+    global sn, mac, pid, ver, max_mac, max_mac_post
+    if "SN" in opthash:
+        sn = opthash["SN"]
+    if "MAC" in opthash:
+        mac = opthash["MAC"]
+    if "ID" in opthash:
+        pid = opthash["ID"]
+    if "VER" in opthash:
+        ver = opthash["VER"]
+    if "MAX_MAC" in opthash:
+        max_mac = opthash["MAX_MAC"]
+    max_mac_post = int(max_mac[6:], 16) if max_mac != "" else 0xFFFFFF
+
+
 def sint6_inc(s):
     i = int(s)
     # debug("sint6_inc: i=" + str(i) + ", i+1=" + str(i + 1) + ", base=" + str(base))
@@ -44,9 +80,10 @@ def sint6_inc(s):
 
 
 def shex6_inc(s):
+    global max_mac_post
     i = int(s, 16)
     # debug("shex6_inc: i=" + str(i) + ", i+1=" + str(i + 1) + ", base=" + str(base))
-    if i == 0xFFFFFF:
+    if i == max_mac_post:
         sys.exit(0)  # overflow
     return "{0:06X}".format(i + 1)
 
@@ -91,41 +128,38 @@ def main_loop():
             break
         crt.Screen.Send("#    \n")
         crt.Sleep(1000)
-        do_product_set()
-        ret = check_result()
-        if ret < 0:
-            break
-        if ret == 1:
+        while True:
+            do_product_set()
+            ret = check_result()
+            if ret < 0:
+                return
+            if ret == 0:
+                continue
             do_info_inc()
+            break
         # crt.Sleep(1000)
 
 
 # init in code
-sn = "202106290001"
-mac = "FCCD2FD00001"
-pid = "LKST-6190-8GT2GS"
-ver = "V1.00"
+# sn = "202106290001"
+# mac = "FCCD2FD00001"
+# pid = "LKST-6190-8GT2GS"
+# ver = "V1.00"
 
-# init by command args
-opthash = parse_opt(crt.Arguments)
-if "sn" in opthash:
-    sn = opthash["sn"]
-if "mac" in opthash:
-    mac = opthash["mac"]
-if "id" in opthash:
-    pid = opthash["id"]
-if "ver" in opthash:
-    ver = opthash["ver"]
-
-# init by prompt input
-sn = crt.Dialog.Prompt("Initial SN", "SN", sn)
-mac = crt.Dialog.Prompt("Initial MAC", "MAC", mac)
-pid = crt.Dialog.Prompt("Initial Product ID", "Product ID", pid)
-ver = crt.Dialog.Prompt("Initial Hardware Version", "Hardware Version", ver)
+# init by args
+opts2args(opt_parse(cmdargs2line(crt.Arguments)))
 
 # init by file? max_mac?
+filepath = crt.Dialog.FileOpenDialog(title="Please select a file", filter="Config Files (*.txt;*.conf)|*.txt;*.conf|All Files(*.*)|*.*")
+filepath != "" and opts2args(opt_parse(property_file2line(filepath)))
+
+# init by prompt input
+# sn = crt.Dialog.Prompt("Initial SN", "SN", sn)
+# mac = crt.Dialog.Prompt("Initial MAC", "MAC", mac)
+# pid = crt.Dialog.Prompt("Initial Product ID", "Product ID", pid)
+# ver = crt.Dialog.Prompt("Initial Hardware Version", "Hardware Version", ver)
 
 if sn != "" and mac != "":
-    crt.Dialog.MessageBox(args2str(), "Initial Args")
+    crt.Dialog.MessageBox(args2str(), "Initial Args", 64)
     main_loop()
-crt.Dialog.MessageBox(args2str(), "Script Exit")
+crt.Dialog.MessageBox(args2str(), "Script Exit", 64)
