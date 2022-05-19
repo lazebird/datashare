3237018081266271cade90f302839e9a6a49cbde
lazebird@163.com <>
2022-05-19 17:27:26 +0800

    feat: update scrt to 9.2.1

--- scrt/linux/crt.sh
+++ scrt/linux/crt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # [Ubuntu18.04下安装 并破解 SecureCRT8.5 的方法记录](https://www.tuziang.com/combat/1237.html)
 
-TARGET=scrt-sfx-8.5.3
+TARGET=scrt-9.2.1
 TARNAME=${TARGET}.tar.gz
-test -f ${TARNAME} || wget -O ${TARNAME} https://code.aliyun.com/lazebird/datashare/raw/master/scrt/linux/scrt-sfx-8.5.3.1867.ubuntu18-64.tar.gz || exit 1
+test -f ${TARNAME} || wget -O ${TARNAME} https://code.aliyun.com/lazebird/datashare/raw/master/scrt/linux/scrt-9.2.1.2768.ubuntu18-64.tar.gz || exit 1
 test -d ${TARGET} || tar -xf ${TARNAME} || exit 1
 
 (test -f libssl.sh || wget https://code.aliyun.com/lazebird/datashare/raw/master/scrt/linux/libssl.sh) || exit 1
@@ -11,17 +11,17 @@ test -d ${TARGET} || tar -xf ${TARNAME} || exit 1
 
 build() {
     sh libssl.sh build
-    sh jpeg.sh build 
-    chmod u+x scrt-sfx-8.5.3/SecureCRT || exit 1
-    mkdir -p /opt/ && mv scrt-sfx-8.5.3/ /opt/ || exit 1
+    sh jpeg.sh build
+    chmod u+x ${TARGET}/SecureCRT || exit 1
+    mkdir -p /opt/ && mv ${TARGET}/ /opt/ || exit 1
     wget https://code.aliyun.com/lazebird/datashare/raw/master/scrt/linux/securecrt_linux_crack.pl || exit 1
-    sudo perl securecrt_linux_crack.pl /opt/scrt-sfx-8.5.3/SecureCRT | tee crt_reg.txt
+    sudo perl securecrt_linux_crack.pl /opt/${TARGET}/SecureCRT | tee crt_reg.txt
 }
 
 clean() {
     sh libssl.sh clean
-    sh jpeg.sh clean 
-    rm -rf /opt/scrt-sfx-8.5.3/
+    sh jpeg.sh clean
+    rm -rf /opt/${TARGET}/
 }
 
 case $1 in
