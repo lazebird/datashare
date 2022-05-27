671c4f2497df4ef25307a69275109d2efaab0345
lazebird <lazebird@gmail.com>
2022-05-27 14:56:05 +0800

    fix: optimize scrt libssl script

--- scrt/linux/libssl.sh
+++ scrt/linux/libssl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # [libssl.so.1.0.0下载安装](https://packages.debian.org/jessie/amd64/libssl1.0.0/download)
+TARGET=libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb
 
 build() {
-    wget https://code.aliyun.com/lazebird/datashare/raw/master/scrt/linux/libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb || exit 1
-    apt install ./libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb -y || exit 1
+    test -f ${TARGET} || wget https://code.aliyun.com/lazebird/datashare/raw/master/scrt/linux/${TARGET} || exit 1
+    apt install ./${TARGET} -y || exit 1
 }
 
 clean() {
